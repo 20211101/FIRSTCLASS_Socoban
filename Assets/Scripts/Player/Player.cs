a1f8dda78b04cb8468a961989c60b03e2e150fba
hms2002 <hmm2002@naver.com>
2025-03-21 13:49:46 +0900

    split stack class

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,38 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-struct MoveData
-{
-    // 이동 전에 위치를 저장 하는 데이터
-    public Vector3 moveDirection;
-    // 이동 할 때 같이 움직인 공이 있으면 해당 오브젝트 정보 담는 데이터
-    public Ball movedBall;
-    public MoveData(Vector3 moveDirection, Ball movedBall)
-    {
-        this.moveDirection = moveDirection;
-        this.movedBall = movedBall;
-    }
-}
-class MoveStack
-{
-    MoveData[] list = new MoveData[100];
-    int top = -1;
-    public bool Add(MoveData data)
-    {
-        if (list.Length <= top) return false;
-        top++;
-        Debug.Log(top);
-        list[top] = data;
-        return true;
-    }
-    public MoveData Pop()
-    {
-        if (top < 0) return new MoveData(Vector3.zero, null);
-        MoveData returnVal = list[top];
-        top--;
-        return returnVal;
-    }
-}
 public class Player : MonoBehaviour
 {
     MoveStack stack = new MoveStack();
